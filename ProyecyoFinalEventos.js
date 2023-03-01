034e8fbf68bb6506bdfba01b6d561b74fe8ed843
MMene <matiasmene@gmail.com>
2023-02-28 23:22:39 -0300

    Primer Commit

---
+++ ProyecyoFinalEventos.js
@@ -0,0 +1,25 @@
+function enAmarillo1() {
+    Element =document.getElementById("pers");
+    Element.src="MMene.github.io/Img/personal2.png";
+    }
+    
+    function enAmarillo2() {
+        Element =document.getElementById("lab");
+        Element.src="MMene.github.io/Img/laboral2.png";
+    }
+
+    function enAmarillo3() {
+        Element =document.getElementById("habili");
+        Element.src="MMene.github.io/Img/habilidades2.png";
+    }
+    
+    function enAmarillo4() {
+        Element =document.getElementById("estud");
+        Element.src="MMene.github.io/Img/estudios2.png";
+    }
+    
+    function MostrarTelefono(){
+        document.getElementById("telefono").innerHTML = "011-2885-9655 // aarongerguson@gmail.com";
+     }
+
+     
