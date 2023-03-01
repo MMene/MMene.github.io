7ff60682c415ef6b3647b0dc0c74b2a7d8f51e28
MMene <matiasmene@gmail.com>
2023-03-01 00:02:36 -0300

    Proyecto Final

--- ProyecyoFinalEventos.js
+++ ProyecyoFinalEventos.js
@@ -1,25 +1,40 @@
 function enAmarillo1() {
     Element =document.getElementById("pers");
-    Element.src="MMene.github.io/Img/personal2.png";
+    Element.src="/MMene.github.io/Img/personal2.png";
+    }
+    function enNegro1() {
+        Element =document.getElementById("pers");
+        Element.src="/MMene.github.io/Img/personal.png";
     }
     
     function enAmarillo2() {
         Element =document.getElementById("lab");
-        Element.src="MMene.github.io/Img/laboral2.png";
+        Element.src="/MMene.github.io/Img/laboral2.png";
     }
-
+     function enNegro2() {
+        Element =document.getElementById("lab");
+        Element.src="/MMene.github.io/Img/laboral.png";
+    }
+    
     function enAmarillo3() {
         Element =document.getElementById("habili");
-        Element.src="MMene.github.io/Img/habilidades2.png";
+        Element.src="/MMene.github.io/Img/habilidades2.png";
+    }
+    function enNegro3() {
+        Element =document.getElementById("habili");
+        Element.src="/MMene.github.io/Img/habilidades.png";
     }
     
     function enAmarillo4() {
         Element =document.getElementById("estud");
-        Element.src="MMene.github.io/Img/estudios2.png";
+        Element.src="/MMene.github.io/Img/estudios2.png";
     }
-    
-    function MostrarTelefono(){
-        document.getElementById("telefono").innerHTML = "011-2885-9655 // aarongerguson@gmail.com";
-     }
+    function enNegro4() {
+        Element =document.getElementById("estud");
+        Element.src="/MMene.github.io/Img/estudios.png";
+    }
 
      
+    function MostrarTelefono(){
+        document.getElementById("telefono").innerHTML = "011-2885-9655 // elenafernandez@hotmail.com.ar";
+     }
